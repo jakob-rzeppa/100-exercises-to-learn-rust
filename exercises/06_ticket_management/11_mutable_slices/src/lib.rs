8feaeab4484e99f462ac50983c98090fc0c598bf
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-12 13:57:09 +0100

    06_11

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+pub fn squared(nums: &mut [i32]) {
+    for number in nums {
+        *number = *number * *number;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
